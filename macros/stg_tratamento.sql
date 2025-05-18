14091956b6eab729d745c83a6e26bfcd1d7954dd
Pedro Lustosa <pedrohenriquelustosa@hotmail.com>
2025-05-18 17:54:19 +0000

    Add função COALESCE na pasta macro para tratar valores NULL.

--- macros/stg_tratamento.sql
+++ macros/stg_tratamento.sql
@@ -9,3 +9,23 @@
 {% macro extrair_hora(nome_coluna) %}
     FORMAT_TIMESTAMP('%H:%M:%S', {{ nome_coluna }}, "America/Sao_Paulo")
 {% endmacro %}
+
+{% macro tratar_null(nome_coluna, tipo) %}
+    {% set tipo = tipo | lower %}
+
+    {% if tipo == 'string' %}
+        coalesce({{ nome_coluna }}, 'N.I')
+
+    {% elif tipo == 'int' %}
+        coalesce({{ nome_coluna }}, 0)
+
+    {% elif tipo == 'float' %}
+        coalesce({{ nome_coluna }}, 0.0)
+
+    {% elif tipo == 'timestamp' %}
+        coalesce({{ nome_coluna }}, '1900-01-01')
+
+    {% else %}
+        {{ nome_coluna }}
+    {% endif %}
+{% endmacro %}
